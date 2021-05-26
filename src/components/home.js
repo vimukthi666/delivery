f05ba65f5bb86940812765afed1b991223a2c1ec
vimukthi666 <vimukthijayasinghe98@gmail.com>
2021-05-26 14:48:08 +0530

    home changes

--- src/components/home.js
+++ src/components/home.js
@@ -11,7 +11,7 @@ export default class Home extends Component{
             <div>
 
             <Form>
-                <p>this is Home</p>
+                <p>Home</p>
                 <Form.Group controlId="email">
                     <Form.Label>Email </Form.Label>
                     <Form.Control type="text" value={this.props.cemail} disabled="enable"/>
